2740ccf72ab7b858f4941fdb1c92947172db633f
Karina Gonzalez <karinamzalez@gmail.com>
2016-06-09 12:08:13 -0600

    added the shared context into rails_helper

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Donation, type: :model do
+  include_context "standard test dataset"
+  it ""
+end
--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -3,5 +3,4 @@ require 'rails_helper'
 RSpec.describe Donation, type: :model do
   it {should belong_to(:user)}
   it {should have_many(:donation_items)}
-
 end
