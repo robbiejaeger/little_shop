5760acf799bb377f1f1512bfb60a480188c85c61
Kerry <kerry.sheldon@gmail.com>
2016-06-28 15:01:57 -0600

    Add factory girl config

---
+++ app/models/category_charity.rb
@@ -0,0 +1,3 @@
+class CategoryCharity < ActiveRecord::Base
+
+end
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
-  factory :category do
-    name
-    slug
+
+  factory :category_charity do
+    name { generate(:category_charity_name) }
   end
-end  
+
+  sequence :category_charity_name, ["Environment", "Youth", "Women"].cycle do |n|
+    "#{n}"
+  end
+
+end
