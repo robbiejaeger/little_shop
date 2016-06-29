76202c22a12b771f36435700f384c22efdb801b1
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-06-29 17:14:24 -0600

    User can view previous orders When logged in, add capability for user to view previous orders, and feature test to ensure that cannot view other orders Rewrite index for donations so that user can see details about donations and that's it. Eliminate old information with families

--- spec/features/user_can_see_donations_spec.rb
+++ spec/features/user_can_see_donations_spec.rb
@@ -1,40 +1,21 @@
 require 'rails_helper'
 
-RSpec.feature "user can see all donations" do
-  scenario "authenticated user sees list of only her donations" do
+RSpec.feature "user can see all donations they have made" do
+  scenario "authenticated user sees list of only her donations when she has one previous donation" do
+    donation, other_users_donation = create_list(:donation, 2)
+    user = User.find(donation.user_id)
+    other_user = User.find(other_users_donation.user_id)
+    visit login_path
 
-    user = User.create(username: "test", password: "password", email: "test@example.com")
-    other_user = User.create(username: "other", password: "password", email: "test@example.com")
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
-
-    nationality = Nationality.create(photo_path: "http://www.criticalthreats.org/sites/default/files/AEI_Somalia_Map_Political.gif" ,info_link: "Somali", greeting: "most-critical", name: "Somali")
-
-    family = Family.create(first_name: "TestFirst", last_name: "TestLast", arrival_date: 10.days.from_now, donation_deadline: 5.days.from_now, nationality: nationality, num_married_adults: 2, num_unmarried_adults: 1, num_children_over_two: 0, num_children_under_two: 0)
-
-    supply = Supply.create(name: "Small Pot", value: 3.0, description: "New or used.", multiplier_type: "household")
-    supply2 = Supply.create(name: "Couch", value: 100.0, description: "New or used.  Used must be in good condition.", multiplier_type: "household")
-    other_supply = Supply.create(name: "Large Pot", value: 3.0, description: "New or used.", multiplier_type: "household")
-
-    supply_item = SupplyItem.create(supply: supply, quantity: 1, family: family)
-    supply_item2 = SupplyItem.create(supply: supply2, quantity: 1, family: family)
-    other_supply_item = SupplyItem.create(supply: other_supply, quantity: 1, family: family)
-
-    donation = Donation.create(status: "Pledged", user: user)
-    other_donation = Donation.create(status: "Pledged", user: other_user)
-
-    donation_item = DonationItem.create(quantity: 1, supply_item: supply_item, donation: donation)
-    donation_item2 = DonationItem.create(quantity: 1, supply_item: supply_item2, donation: donation)
-    other_donation_item = DonationItem.create(quantity: 1, supply_item: other_supply_item, donation: other_donation)
+    fill_in "Username", with: "#{user.username}"
+    fill_in "Password", with: "password"
+    click_on "Login to Account"
 
     visit donations_path
-
     expect(page).to have_content("#{user.username}'s Donations")
     expect(page).to have_content("Total Donations: 1")
-    expect(page).to have_content("Somali family of 3")
-    expect(page).to have_content("1 Small Pot")
-    expect(page).to have_content("1 Couch")
-    expect(page).to_not have_content("1 Large Pot")
+    expect(page).to have_link("#{donation.id}", href: "/donations/#{donation.id}")
+    expect(page).to_not have_link("#{other_users_donation.id}", href: "/donations/#{other_users_donation.id}")
   end
 
   scenario "guest user visiting donations is redirected to log in" do
