e7d0ea04d42cdda7a435a1893b018cd503fe8094
Kerry <kerry.sheldon@gmail.com>
2016-06-28 18:06:22 -0600

    Test factories
    Adds factory_spec.rb in feature test directory to ensure factories
    operating as intended.  Spec file also demonstrates how to develop
    items using factories

---
+++ spec/features/factory_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+RSpec.feature  "testing factories" do
+  scenario "test it makes a cause" do
+    cause = create(:cause)
+    expect(cause.name).to eq("1 Cause")
+    expect(Cause.count).to eq(1)
+  end
+
+  scenario "test it makes a charity" do
+    charity_one, charity_two, charity_three = create_list(:charity, 3)
+    expect(charity_two.name).to eq("2 Charity")
+    expect(charity_two.description).to eq("This is description for 2 Charity")
+    expect(Charity.count).to eq(3)
+  end
+
+  scenario "test it makes a needs_category" do
+    needs_category = create(:needs_category)
+    expect(needs_category.name).to eq("1 Needs Category")
+    expect(NeedsCategory.count).to eq(1)
+  end
+
+  scenario "test it makes a need" do
+    need_one, need_two = create_list(:need, 2)
+    expect(need_one.name).to eq("1 Need")
+    expect(need_one.description).to eq("This is description for 1 Need")
+    expect(need_one.needs_category).to be_a_kind_of(NeedsCategory)
+    expect(need_two.needs_category).to be_a_kind_of(NeedsCategory)
+    expect(need_one.price).to eq(10.0)
+    expect(need_two.price).to eq(20.0)
+    expect(Need.count).to eq(2)
+  end
+
+  scenario "test it makes a user" do
+    user_one, user_two = create_list(:user, 2)
+    expect(user_one.username).to eq("user1")
+    expect(user_two.username).to eq("user2")
+    expect(user_one.password).to eq("password")
+    expect(user_one.email).to eq("fake@fake.com")
+  end
+
+  scenario "test it makes a recipient" do
+    recipient_one, recipient_two = create_list(:recipient, 2)
+    expect(recipient_one.name).to eq("1 Recipient")
+    expect(recipient_one.description).to eq("This is description for 1 Recipient")
+    expect(recipient_one.charity).to be_a_kind_of(Charity)
+    expect(Recipient.count).to eq(2)
+  end
+
+  scenario "test it makes a future need item" do
+    item = create(:future_need_item)
+    expect(item.quantity).to eq(1)
+    expect(item.need).to be_a_kind_of(Need)
+    expect(item.recipient).to be_a_kind_of(Recipient)
+    expect(item.deadline).to be > Date.today
+    expect(NeedItem.count).to eq(1)
+  end
+
+  scenario "test it makes a past need item" do
+    item = create(:past_need_item)
+    expect(item.quantity).to eq(1)
+    expect(item.need).to be_a_kind_of(Need)
+    expect(item.recipient).to be_a_kind_of(Recipient)
+    expect(item.deadline).to be < Date.today
+    expect(NeedItem.count).to eq(1)
+  end
+
+  scenario "test it a donation" do
+    donation = create(:donation)
+    expect(donation.user).to be_a_kind_of(User)
+    expect(Donation.count).to eq(1)
+  end
+
+
+
+end
