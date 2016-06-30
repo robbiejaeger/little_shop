a806f9ee6e4c0cd90407e8a72271cf3afa5ab7b7
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-06-30 14:45:14 -0600

    Closes #48 allow logged in user to checkout User has ability to check out a donation Add happy and sad path test to check logged in user can check out Non-logged in user cannot check out

--- spec/features/user_can_checkout_spec.rb
+++ spec/features/user_can_checkout_spec.rb
@@ -2,64 +2,39 @@ require 'rails_helper'
 
 RSpec.feature "user can checkout with items in cart" do
   scenario "they checkout and see the donations page with their donation" do
-    user = User.create(username: "test", password: "password", email: "email@example.com")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
-
-    nationality = Nationality.create(photo_path: "http://www.criticalthreats.org/sites/default/files/AEI_Somalia_Map_Political.gif",
-        info_link: "Somali", greeting: "most-critical", name: "Somali")
-    family = Family.create(first_name: "TestFirst", last_name: "TestLast",
-        arrival_date: 10.days.from_now, donation_deadline: 5.days.from_now,
-        nationality: nationality, num_married_adults: 2, num_unmarried_adults: 1,
-        num_children_over_two: 0, num_children_under_two: 0)
-    supply = Supply.create(name: "Small Pot", value: 3.0,
-        description: "New or used.", multiplier_type: "household")
-    supply_item = SupplyItem.create(supply: supply, quantity: 1, family: family)
-
-    visit family_path(family)
-
-    click_on "add to cart"
-
-    visit cart_index_path
-
-    click_on "Checkout"
-
-    expect(page).to have_content "Small Pot"
-    expect(page).to have_content "1"
-
-    click_on "Confirm Donation"
-
-    expect(page).to have_content "Your donation (ID#: 1) was recieved. Thank you!"
-    expect(page).to have_content "Somali family of 3"
-    expect(page).to have_content "1 Small Pot"
-    expect(page).to have_content "Pledged"
-  end
-
-  scenario "logged out user cannot checkout unless logged in" do
-    user = User.create(username: "test", password: "password", email: "email@example.com")
-
-    nationality = Nationality.create(photo_path: "http://www.criticalthreats.org/sites/default/files/AEI_Somalia_Map_Political.gif",
-        info_link: "Somali", greeting: "most-critical", name: "Somali")
-    family = Family.create(first_name: "TestFirst", last_name: "TestLast",
-        arrival_date: 10.days.from_now, donation_deadline: 5.days.from_now,
-        nationality: nationality, num_married_adults: 2, num_unmarried_adults: 1,
-        num_children_over_two: 0, num_children_under_two: 0)
-    supply = Supply.create(name: "Small Pot", value: 3.0,
-        description: "New or used.", multiplier_type: "household")
-    supply_item = SupplyItem.create(supply: supply, quantity: 1, family: family)
-
-    visit family_path(family)
-
-    click_on "add to cart"
-
-    visit cart_index_path
-
-    click_on "Login or Create Account to Checkout"
-
+    user = User.create(username: "test", password: "password", email: "test@example.com" )
+    visit login_path
     fill_in "Username", with: "test"
     fill_in "Password", with: "password"
     click_on "Login to Account"
 
-    expect(page).to have_content "Small Pot"
-    expect(page).to have_content "Total: $3.00"
+    need = create(:need)
+    charity = create(:charity)
+    recipient = Recipient.create(name: "worthy recipient", description: "all about the worthy recipient", charity_id: charity.id)
+    need_item = NeedItem.create(quantity: 1, recipient_id: recipient.id,  need_id: need.id, created_at: Faker::Date.backward(10), updated_at: Faker::Date.backward(4), deadline: Faker::Date.forward(14))
+
+    visit charity_recipient_path(recipient.charity.slug, recipient)
+    click_on "add to cart"
+
+    visit cart_index_path
+    click_on "Checkout"
+    click_on "Confirm Donation"
+
+    expect(page).to have_content "Your donation, with ID 1, was received. Thank you!"
+    expect(page).to have_content "#{need.name}"
+    expect(page).to have_content "Total Donations: 1"
+  end
+
+  scenario "logged out user cannot checkout unless logged in" do
+    need = create(:need)
+    charity = create(:charity)
+    recipient = Recipient.create(name: "worthy recipient", description: "all about the worthy recipient", charity_id: charity.id)
+    need_item = NeedItem.create(quantity: 1, recipient_id: recipient.id,  need_id: need.id, created_at: Faker::Date.backward(10), updated_at: Faker::Date.backward(4), deadline: Faker::Date.forward(14))
+
+    visit charity_recipient_path(recipient.charity.slug, recipient)
+    click_on "add to cart"
+
+    visit cart_index_path
+    expect(page).to have_content "Login or Create Account to Checkout"
   end
 end
