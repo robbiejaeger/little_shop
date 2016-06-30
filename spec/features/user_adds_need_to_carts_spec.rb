37ee159ee761e43290a7fe13ac136f36ff04bf1f
Kerry <kerry.sheldon@gmail.com>
2016-06-29 18:56:23 -0600

    Add all cart functionality
    Add to cart from multiple charities
    Update cart
    Remove item from cart

--- spec/features/user_adds_need_to_carts_spec.rb
+++ spec/features/user_adds_need_to_carts_spec.rb
@@ -23,34 +23,37 @@ RSpec.feature "User Adds Need To Carts" do
   scenario "cart has need that user added for multiple charities recipients" do
 
     charity_one, charity_two = create_list(:charity, 2)
-    recipient_one, recipient_two = create_list(:recipient, 2)
-    recipient_one.charity = charity_one
-    recipient_two.charity = charity_two
-    recipient_one.need_items.create(quantity: 2, need: create(:need), deadline: 5.days.from_now)
-    recipient_two.need_items.create(quantity: 8, need: create(:need), deadline: 5.days.from_now)
+    recipient_one = charity_one.recipients.create(name: "rec1", description: "test1")
+    recipient_two = charity_two.recipients.create(name: "rec2", description: "test1")
+    item_one = recipient_one.need_items.create(quantity: 2, need: create(:need), deadline: 5.days.from_now)
+    item_two = recipient_two.need_items.create(quantity: 8, need: create(:need), deadline: 5.days.from_now)
 
     visit charity_recipient_path(charity_one.slug, recipient_one)
 
-    within(".#{recipient_one.need_items.first.name}") do
+    within(".#{item_one.name}") do
       select  1, from: "need_item[quantity]"
       find(:button, "add to cart").click
     end
 
     visit charity_recipient_path(charity_two.slug, recipient_two)
 
-    within(".#{recipient_two.need_items.first.name}") do
+    within(".#{item_two.name}") do
       select  1, from: "need_item[quantity]"
       find(:button, "add to cart").click
     end
 
     visit cart_index_path
 
-    expect(page).to have_content("#{recipient_one.need_items.first.name}")
-    expect(page).to have_content("#{recipient_two.need_items.first.name}")
+    expect(page).to have_content("#{item_one.name}")
+    expect(page).to have_content("#{item_two.name}")
 
-    #the charity names are wrong...
     click_on "#{recipient_one.charity.name}"
 
     expect(current_path).to eq(charity_path(recipient_one.charity.slug))
+
+    visit cart_index_path
+    click_on "#{item_two.name}"
+    expect(current_path).to eq(charity_recipient_path(charity_two.slug, recipient_two))
+
   end
 end
