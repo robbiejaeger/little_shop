62071bc2761f3cb9efae7b6e0d7b1d8cad5706c0
Kerry <kerry.sheldon@gmail.com>
2016-07-02 10:31:13 -0600

    fixed one test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password) }
-  it { should validate_presence_of(:role) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:username) }
   it { should validate_uniqueness_of(:email) }
