b1fce7f50540d7a94dee3143eeeb1f3d6559f055
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-07-02 19:17:02 -0600

    Update model tests to reflect current schema When appropriate, delete and add new tests If test may be helpful in future comment out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,13 +5,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:password) }
   it { should validate_presence_of(:role) }
   it { should validate_presence_of(:email) }
-
-
-  it do
-    should_not allow_values('aaabbbcccc', '111-222-3333').for(:cell)
-  end
-
-  it { should allow_value('1112223333').for(:cell) }
-
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:email) }
   it { should have_secure_password }
 end
